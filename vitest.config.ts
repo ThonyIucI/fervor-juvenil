dd816362b03e228cb7fe804c9791afb08d160e28
thony <desarrollo2.punku@gmail.com>
2025-10-25 03:06:17 -0500

    feat: fommatting settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,20 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/tests/setup.ts'],
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'src/tests/', '**/*.d.ts', '**/*.config.*', '**/mockData', 'dist/']
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -16,15 +22,6 @@ export default defineConfig({
       '@modules': path.resolve(__dirname, './src/modules'),
       '@config': path.resolve(__dirname, './src/config'),
       '@tests': path.resolve(__dirname, './src/tests')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
     }
   }
 })
